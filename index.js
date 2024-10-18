c7afc4b909d534176197ec7e252749fb04dee9fc
albertoojeda-creater <alberto.ojeda.ao2001@gmail.com>
2024-10-18 07:39:07 -0600

    Cambios e implementación con Front

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const usuariosRutas = require("./routes/rutasUsuarios");
 const productosRutas = require("./routes/rutasProductos");
 const ventasRutas = require("./routes/rutasVentas");
@@ -9,6 +10,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(cors());
 // Cargar rutas
 app.use("/", usuariosRutas);
 app.use("/productos", productosRutas); // Cargar rutas de productos
